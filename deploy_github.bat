55071360437e94e094ec846179c08f5f433bf61a
DESKTOP-HNVKEBO\emuser <emadmahdi@yahoo.com>
2022-12-11 16:53:25 -0500

    commit from cli

--- deploy_github.bat
+++ deploy_github.bat
@@ -2,6 +2,9 @@
 cd /d C:\DEVS\github_KODI
 
 git add . && git commit -m "commit from cli" && git push -u origin main
+
+git add . && git commit -m "commit from cli" && git push -u origin master
+
 cmd
 
 
